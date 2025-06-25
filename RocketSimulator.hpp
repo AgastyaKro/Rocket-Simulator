486db992ea206b1ed5091ea3f8a4810acbcf2fbe
Agastya Krothapalli <agastyakro06@gmail.com>
2025-06-24 19:22:09 -0500

    wrote out .hpp for RocketSimulation

---
+++ RocketSimulator.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "RocketStage.hpp"
+#include <fstream>
+
+
+class RocketSimulator{
+
+public:
+    RocketSimulator(RocketStage stage, double dt);
+    void run_simulation(std::ostream& out);
+
+private:
+    RocketStage stage;
+    double dt;
+    double time;
+    double position;
+    double velocity;
+
+    void log_state(std::ostream& out, double mass, double acceleration);
+
+};
