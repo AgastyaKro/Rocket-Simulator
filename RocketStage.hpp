cdb999354e78e85fbff49aea82d7c4db2513b6b2
Agastya Krothapalli <agastyakro06@gmail.com>
2025-06-28 20:36:49 -0500

    changed RocketStage to be a struct w a constructor

--- RocketStage.hpp
+++ RocketStage.hpp
@@ -7,6 +7,9 @@ struct RocketStage{
     double dry_mass;    // kg
     double isp;         // seconds 
 
+    RocketStage(double thrust_, double burn_time_, double fuel_mass_, double dry_mass_, double isp_)
+            : thrust(thrust_), burn_time(burn_time_), fuel_mass(fuel_mass_), dry_mass(dry_mass_), isp(isp_) {}
+
     double get_total_mass(){ 
         return dry_mass + fuel_mass;
     }
