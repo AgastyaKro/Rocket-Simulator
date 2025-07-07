74be323ea10f95ccb9825d9e5eccad81a9aaef58
Agastya Krothapalli <agastyakro06@gmail.com>
2025-07-06 20:04:30 -0500

    Re-wrote some logic to incoporate mass flow rate

--- RocketSimulator.cpp
+++ RocketSimulator.cpp
@@ -1,6 +1,6 @@
 #include "RocketSimulator.hpp"
 
-const double GRAVITY = 9.81;
+constexpr double GRAVITY = 9.81;
 
 RocketSimulator::RocketSimulator(RocketStage stage, double dt)  : stage(stage), dt(dt), time(0.0), position(0.0), velocity(0.0){}
 
@@ -11,21 +11,36 @@ void RocketSimulator::log_state(std::ostream& out, double mass, double accelerat
 void RocketSimulator::run_simulation(std::ostream& out){
     out << "time,position,velocity,acceleration,mass\n";
 
+    double fuel_mass = stage.fuel_mass;           // track fuel separately
+    double dry_mass = stage.dry_mass;
+    double mass;
+
     while (position >= 0) {
-        double mass = stage.get_mass(time);
-        double thrust = stage.get_thrust(time);
+        // Compute mass and thrust
+        mass = dry_mass + fuel_mass;
+        double thrust = (fuel_mass > 0 && time <= stage.burn_time) ? stage.thrust : 0.0;
+
+        // Burn fuel if burning
+        if (thrust > 0.0) {
+            double mass_flow_rate = stage.get_mass_flow_rate();
+            fuel_mass -= mass_flow_rate * dt;
+            if (fuel_mass < 0.0) fuel_mass = 0.0;
+        }
+
+        // Forces
         double weight = mass * GRAVITY;
         double net_force = thrust - weight;
         double acceleration = net_force / mass;
 
+        // Integration
         velocity += acceleration * dt;
         position += velocity * dt;
 
+        // Log
         log_state(out, mass, acceleration);
         time += dt;
 
+        // Stop at peak after burn
         if (velocity <= 0 && time > stage.burn_time) break;
-
     }
 }
-
--- RocketStage.hpp
+++ RocketStage.hpp
@@ -1,35 +1,28 @@
 #pragma once
 
+constexpr double GRAVITY = 9.81;
+
 struct RocketStage{
-    double thrust;      // Newtons
+    double thrust;      // newtons
     double burn_time;   // seconds
     double fuel_mass;   // kg
     double dry_mass;    // kg
     double isp;         // seconds 
 
     RocketStage(double thrust_, double burn_time_, double fuel_mass_, double dry_mass_, double isp_)
-            : thrust(thrust_), burn_time(burn_time_), fuel_mass(fuel_mass_), dry_mass(dry_mass_), isp(isp_) {}
+            : thrust(thrust_), burn_time(burn_time_), fuel_mass(fuel_mass_), 
+              dry_mass(dry_mass_), isp(isp_) {}
 
     double get_total_mass(){ 
         return dry_mass + fuel_mass;
     }
 
-    double get_mass(double time){
-        if (time > burn_time){
-            return dry_mass; // all fuel should have been burned 
-        }
-        double fuel_burned = (fuel_mass / burn_time) * time;
-        return dry_mass + fuel_mass - fuel_burned;
+    double get_mass_flow_rate() const {
+        return thrust / (isp * GRAVITY);
     }
 
     double get_thrust(double time){
-        
         return (time <= burn_time ? thrust : 0); // no fuel after burn_time so no thrust
     }
-
-
-
-
-
 
 };
