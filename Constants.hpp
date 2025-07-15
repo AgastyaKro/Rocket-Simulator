d7588182518c92dcadfb4510b18beaa6458ae230
Agastya Krothapalli <agastyakro06@gmail.com>
2025-07-14 20:41:11 -0500

    New constants

--- Constants.hpp
+++ Constants.hpp
@@ -7,3 +7,5 @@ const double DEFAULT_ENV_TEMP     = 288.15;  // Kelvin
 const double DEFAULT_ENV_PRESSURE = 101325;  // Pascals
 const double DEFAULT_ENV_ALTITUDE = 0.0;     // Meters
 const double DEFAULT_ENV_DENSITY  = 1.225;   // kg/m³
+const double M = 0.0289644;      // kg/mol (dry air)
+const double R = 8.3144598;      // J/mol·K
